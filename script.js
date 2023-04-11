0ec61d269307fd6907fdc4132b84d970500ed7b1
Saurabh Bisht <76562057+sbisht01-dev@users.noreply.github.com>
2023-04-11 17:04:33 +0530

    day 3

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ window.onload = function (herotext, rimg, limg) {
     herotext.style.marginTop = value * 1.2 + "px";
     console.log(value);
   });
-};
+
+  let gojo_front_white = "/resource/products//gojo front white post";
+  let post_ig2 = "/resource/products/post ig 2";
+  let luffy_over_size_black =
+    "/resource/products/luffy over size post black front";
+  let 
+    
+    function image (){
+
+    }
+    let image  =  document.querySelector('.display')
+    image.innerHTML = '<img class="img" src="/resource/products/gojo front white post.jpg"></img>'
+  };
